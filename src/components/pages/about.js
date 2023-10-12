558da1352742058292d4535969b0ba520d0e4c8d
aleyesa <aaron.leyesa@gmail.com>
2023-10-12 11:03:57 -0700

    Updated Portfolio

---
+++ src/components/pages/about.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import profilePicture from "../../../static/assets/images/bio/pic1.jpg";
+
+export default function() {
+    return (
+        <div className='content-page-wrapper'>
+            <div 
+                className='left-column'
+                style={{
+                    background: "url(" + profilePicture + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                }}
+
+            />
+            <div className='right-column'>
+                safasf.
+                asdfsaf.
+                asfdasfasdfas.
+                fin
+            </div>
+        </div>
+    );
+}
